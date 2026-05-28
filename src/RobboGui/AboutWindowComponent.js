eaeabe89e3c370b40ac35141a2b96e1c27f09614
ROBBOSoftwareTeamlead <software@robbo.world>
2026-05-28 18:49:26 +0300

    fix(gui): вкладка скрытия палитры на краю flyout
    Оформление кнопки как продолжения палитры; версия About 3.119.5;
    настройки webpack для web dev-сервера.

--- src/RobboGui/AboutWindowComponent.js
+++ src/RobboGui/AboutWindowComponent.js
@@ -20,7 +20,7 @@ import {
   formatArchitectureDisplay
 } from '../lib/platform';
 
-const VERSION = 'Robbo Scratch v.3.119.4';
+const VERSION = 'Robbo Scratch v.3.119.5';
 const BUILD_VERSION_SUFFIX = (typeof process !== 'undefined' &&
   process &&
   process.env &&
