d88ee89bfa0338ed1a3e9f821538b1742f8aebdc
ROBBOSoftwareTeamlead <software@robbo.world>
2026-05-29 17:18:32 +0300

    chore: bump About version to 3.120.4

--- src/RobboGui/AboutWindowComponent.js
+++ src/RobboGui/AboutWindowComponent.js
@@ -20,7 +20,7 @@ import {
   formatArchitectureDisplay
 } from '../lib/platform';
 
-const VERSION = 'Robbo Scratch v.3.120.3';
+const VERSION = 'Robbo Scratch v.3.120.4';
 const BUILD_VERSION_SUFFIX = (typeof process !== 'undefined' &&
   process &&
   process.env &&
