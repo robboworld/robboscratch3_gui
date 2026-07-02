47c867dd7fdd140cbeb320f0b54e684e09037761
ROBBOSoftwareTeamlead <software@robbo.world>
2026-07-02 19:16:14 +0300

    chore(gui): sync About window version to 3.126.3

--- src/RobboGui/AboutWindowComponent.js
+++ src/RobboGui/AboutWindowComponent.js
@@ -37,7 +37,7 @@ const COPY_FEEDBACK_TOKEN = 'copied';
 const COPY_BUTTON_FEEDBACK_KEY = 'copyButtonFeedback';
 const SUPPORT_URL = 'https://support.robbo.world/';
 
-const VERSION = 'Robbo Scratch v.3.126.0';
+const VERSION = 'Robbo Scratch v.3.126.3';
 export {VERSION};
 export const APP_VERSION = (VERSION.match(/v\.(.+)$/) || [])[1] || '';
 const BUILD_VERSION_SUFFIX = (typeof process !== 'undefined' &&
